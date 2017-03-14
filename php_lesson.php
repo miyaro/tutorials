6169770f187b0e0c83ca1623f3bd53547d04308a
miyaro <iddsk1234@gmail.com>
2017-03-14 05:33:57 +0000

    initial_repository

---
+++ php_lesson.php
@@ -0,0 +1,11 @@
+<?php
+
+$sales = [
+    "taguchi"  => 200,
+    "miyaguchi" => 1000,
+    "yamada" => 300,
+    ];
+    
+    foreach($sales as $key => $value){
+        echo"($key)$value";
+        }
