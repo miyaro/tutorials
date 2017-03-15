76c8c7604c2fc7f2530bc9bd4bccc262dec296e7
miyaro <iddsk1234@gmail.com>
2017-03-15 09:58:52 +0000

    Todoクラスと連携

--- TodoApp/index.php
+++ TodoApp/index.php
@@ -5,41 +5,35 @@ require_once(__DIR__ . '/functions.php');
 require_once(__DIR__ . '/Todo.php');
 
 //get todos
-
 $todoApp = new \MyApp\Todo();
-$todo = $todoApp->getAll();
+$todos = $todoApp->getAll();
 
-
-var_dump($todos);
-exit;
+// var_dump($todos);
+// exit;
 ?>
-
-
-
 <!DOCTYPE html>
-<html>
-
+<html lang ="ja">
 <head>
     <meta charset = "utf-8">
     <title>My Todo</title>
     <link rel = "stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Todos</h1>
-    <form action="">
-        <input type="text" placeholder="what needs to be done?" id = "new_todo">
-    </form>
-    <ul>
-        <li>
-            <input type ="checkbox" >
-            <span>Do something</span>
-        </li>
-        <div class = "delete_todo">x</div>
-        <li>
-            <input type ="checkbox" checked>
-            <span class="done">Do something</span>
-        </li>
-        <div class = "delete_todo">x</div>
-    </ul>
+    <div id ="container">
+        <h1>Todos</h1>
+        <form action="">
+            <input type="text" placeholder="what needs to be done?" id = "new_todo">
+        </form>
+        <ul>
+        <?php foreach ($todos as $todo) : ?>
+            <li>
+                <input type ="checkbox" <?php if($todo->state === '1'){echo 'checked';}?>>
+                <span class=" <?php if($todo->state === '1') { echo 'done'; } ?>"><?= h($todo->title); ?>
+                </span>
+                <div class = "delete_todo">x</div>
+            </li>        
+            <?php endforeach;?> 
+        </ul>
+    </div><!--container-->
 </body>
 </html>
