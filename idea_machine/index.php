fb35ae3240bd6d4377cc4fc7c8860f0befe70d67
miyaro <iddsk1234@gmail.com>
2017-05-18 01:08:12 +0000

    ideamachineの雛形作成

---
+++ idea_machine/index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>idea_machine</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<table border="1" class="output">
+		<tr>
+			<td>Who(Target)</td>
+			<td>What</td>
+			<td>Place</td>
+			<td>Price</td>
+			<td>Promotion</td>
+		</tr>
+		<tr>
+			<td>sample1</td>
+			<td>sample2</td>
+			<td>sample3</td>
+			<td>sample4</td>
+			<td>sample5</td>
+		</tr>
+	</table>
+	<div class="side_bar">
+	</div>
+	<form action="" method="post" class="submit_button">
+		<input type="button" name="reflesh" value="更新！" onclick="location.replace('/')">
+	</form>
+
+</body>
+</html>
